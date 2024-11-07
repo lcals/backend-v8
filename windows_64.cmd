549bef79c3452db305baec991bfd6b72114b758c
梁铖 <cheng_liang@meta48.com>
2024-11-07 16:04:09 +0800

    test

--- windows_64.cmd
+++ windows_64.cmd
@@ -71,16 +71,17 @@ node %~dp0\node-script\patchs.js . %VERSION% %NEW_WRAP%
 
 echo =====[ Building V8 ]=====
 if "%VERSION%"=="11.8.172" (
-    call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false v8_static_library=true %CXX_SETTING% strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false v8_enable_system_instrumentation=false"
+    call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=true v8_static_library=true %CXX_SETTING% strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false v8_enable_system_instrumentation=false"
 )
 
 if "%VERSION%"=="10.6.194" (
-    call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false v8_static_library=true %CXX_SETTING% strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false v8_enable_sandbox=false v8_enable_system_instrumentation=false"
+    call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=true v8_static_library=true %CXX_SETTING% strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false v8_enable_sandbox=false v8_enable_system_instrumentation=false"
 )
 
 if "%VERSION%"=="9.4.146.24" (
-    call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false v8_static_library=true %CXX_SETTING% strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false v8_enable_system_instrumentation=false"
+    call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=true v8_static_library=true %CXX_SETTING% strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false v8_enable_system_instrumentation=false"
 )
+
 call ninja -C out.gn\x64.release -t clean
 call ninja -v -C out.gn\x64.release wee8
 
@@ -88,6 +89,7 @@ md output\v8\Lib\Win64
 if "%NEW_WRAP%"=="with_new_wrap" (
   call %~dp0\rename_symbols_win.cmd x64 output\v8\Lib\Win64\
 )
+dir out.gn\x64.release\obj\ /b /s
 copy /Y out.gn\x64.release\obj\wee8.lib output\v8\Lib\Win64\
 
 echo =====[ Copy V8 header ]=====
