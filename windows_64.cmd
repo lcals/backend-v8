b93f220eaf5474d2886ff1e07c55357b403bfe0c
Lcals <liangchengandliushui@gmail.com>
2025-04-02 19:01:46 +0800

    Update windows_64.cmd

--- windows_64.cmd
+++ windows_64.cmd
@@ -79,7 +79,7 @@ if "%VERSION%"=="10.6.194" (
 )
 
 if "%VERSION%"=="9.4.146.24" (
-    call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false v8_static_library=true %CXX_SETTING% strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false v8_enable_system_instrumentation=false"
+    call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false v8_static_library=true %CXX_SETTING% strip_debug_info=true symbol_level=2 v8_enable_pointer_compression=false v8_enable_system_instrumentation=false"
 )
 call ninja -C out.gn\x64.release -t clean
 call ninja -v -C out.gn\x64.release wee8
