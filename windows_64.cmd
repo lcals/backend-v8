b9d3537eb4679d0d7be9323c91c39150ee53a564
lcalswin <lcalswin@gmail.com>
2026-03-14 15:48:34 +0800

    Revert "fix(ci): fallback to v8_monolith on windows"
    This reverts commit a909a8c693c5c179a944948a39e28503a37d291c.

--- windows_64.cmd
+++ windows_64.cmd
@@ -88,24 +88,16 @@ if "%VERSION%"=="9.4.146.24" (
 ) else if "%VERSION%"=="11.8.172" (
     call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false v8_static_library=true %CXX_SETTING% strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false v8_enable_system_instrumentation=false"
 ) else (
-call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false v8_static_library=true is_clang=true use_custom_libcxx=false strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false v8_enable_system_instrumentation=false cppgc_enable_caged_heap=false"
+    call gn gen out.gn\x64.release -args="target_os=""win"" target_cpu=""x64"" v8_use_external_startup_data=false v8_enable_i18n_support=false is_debug=false v8_static_library=true is_clang=true use_custom_libcxx=false strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false v8_enable_system_instrumentation=false cppgc_enable_caged_heap=false"
 )
 call ninja -C out.gn\x64.release -t clean
 call ninja -v -C out.gn\x64.release wee8
-if errorlevel 1 (
-    echo =====[ fallback to v8_monolith ]=====
-    call ninja -v -C out.gn\x64.release v8_monolith
-)
 
 md output\v8\Lib\Win64
 if "%NEW_WRAP%"=="with_new_wrap" (
   call %~dp0\rename_symbols_win.cmd x64 output\v8\Lib\Win64\
 )
-if exist out.gn\x64.release\obj\wee8.lib (
-  copy /Y out.gn\x64.release\obj\wee8.lib output\v8\Lib\Win64\
-) else (
-  copy /Y out.gn\x64.release\obj\v8_monolith.lib output\v8\Lib\Win64\wee8.lib
-)
+copy /Y out.gn\x64.release\obj\wee8.lib output\v8\Lib\Win64\
 
 echo =====[ Copy V8 header ]=====
 xcopy include output\v8\Inc\  /s/h/e/k/f/c
